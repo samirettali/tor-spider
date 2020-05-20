ac31701bb44d0aaaf331ed0eb31b44896c8bdde9
Samir Ettali <ettali.samir@gmail.com>
2020-05-20 22:33:09 +0200

    Added SavePageError

---
+++ savepageerror.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+// SavePageError is an error used to signal that there are no jobs in the storage
+type SavePageError struct {
+	Err string
+}
+
+func (e *SavePageError) Error() string {
+	return fmt.Sprintf(e.Err)
+}
