80b8a75d4c1971e00f8330bb16908fac13f0d8b6
Samir Ettali <ettali.samir@gmail.com>
2020-05-29 16:34:41 +0200

    Added seed script

---
+++ seed.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl 'http://localhost:8080?url=https://www.reddit.com/r/onions'
+curl 'http://localhost:8080?url=http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page'
