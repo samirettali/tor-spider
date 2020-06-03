c35c56978f0a30a40d871d0db5636b2b0a8ff369
Samir Ettali <ettali.samir@gmail.com>
2020-06-03 05:57:39 +0200

    Updated seed script

--- seed.sh
+++ seed.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-curl 'http://localhost:8080?url=https://www.reddit.com/r/onions'
-curl 'http://localhost:8080?url=http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page'
+curl 'http://localhost:8080/seed?url=http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page'
